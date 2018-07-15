cec396b788edb592e676a4d9fb32ca208dd25792
Tasos Temperekidis <tasosxak@hotmail.com>
2018-07-15 14:43:44 +0300

    Update nectar.c

--- nectar-lang/nectar.c
+++ nectar-lang/nectar.c
@@ -22,15 +22,43 @@ int main(int argc, char* argv[]){
 	char str[255];
 	int error = 0;
 	if(strcmp(argv[1],"create") == 0){
+		
+		#ifdef __linux__
 		sprintf(str,"rm -r -f %s",argv[2]);
+		#ifdef _WIN32
+		sprintf(str,"rmdir %s",argv[2]);
+		#endif
 		system(str);
+		
+		#ifdef __linux__
 		sprintf(str,"mkdir %s",argv[2]);
+		#ifdef _WIN32
+		sprintf(str,"mkdir %s",argv[2]);
+		#endif
 		system(str);
+		
+		#ifdef __linux__
 		sprintf(str,"mkdir %s/src",argv[2]);
+		#ifdef _WIN32
+		sprintf(str,"mkdir %s/src",argv[2]);
+		#endif
 		system(str);
+		
+		#ifdef __linux__
 		sprintf(str,"mkdir %s/exec",argv[2]);
+		#ifdef _WIN32
+		sprintf(str,"mkdir %s/exec",argv[2]);
+		#endif
 		system(str);
+		
+		#ifdef __linux__
 		sprintf(str,"echo \' func main() -> int { \n \n puts \"Hello world!\"\n\nreturn 0\n}\'>>%s/src/%s.nr",argv[2],argv[2]);
+		#ifdef _WIN32
+		sprintf(str,"@echo func main() -> int { >%s/src/%s.nr",argv[2],argv[2]);
+		sprintf(str,"@echo puts \"Hello world!\">%s/src/%s.nr",argv[2],argv[2]);
+		sprintf(str,"@echo return 0 >%s/src/%s.nr",argv[2],argv[2]);
+		sprintf(str,"@echo } >%s/src/%s.nr",argv[2],argv[2]);
+		#endif
 		system(str);
 	}
 	else if(strcmp(argv[1],"build") == 0){
