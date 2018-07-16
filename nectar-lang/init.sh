2f603405efc970d6d9b3043e1a136ee9da4c3627
tasosxak <tasosxak@hotmail.com>
2018-07-16 05:24:05 +0300

    rem

--- nectar-lang/init.sh
+++
@@ -1,3 +0,0 @@
-yacc -dv nectar_syntax
-flex nectar_lexer.l
-gcc -Wall lex.yy.c y.tab.c nectar_semantics.c -o nectarc
--- nectar-lang/zyywrap.c
+++
@@ -1,9 +0,0 @@
-int yywrap(void) {
-    return 1;
-}
-
-int main(void) {
-    yylex();
-    return 0;
-}
-
